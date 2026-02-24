49ac267f94eda7e186eb8b8dd43f035ecca97daf
mohamedkeshawy <mokeshawy@gmail.com>
2026-02-24 08:19:25 +0200

    update built number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.madarsoft_task"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
